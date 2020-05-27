793ef148a7c72edb388cc2f2f938ca7552698ae6
Gaurav Agrawal <gagrawal3@apple.com>
2020-05-27 16:41:23 +0530

    final commit

---
+++ datatool-client/src/main/java/com/example/datatoolclient/testclient/DTClient.java
@@ -0,0 +1,5 @@
+package com.example.datatoolclient.testclient;
+
+public class DTClient {
+
+}
--- eureka-client-app1/src/main/java/com/ga/app1/controller/AppController.java
+++ eureka-client-app1/src/main/java/com/ga/app1/controller/AppController.java
@@ -3,9 +3,14 @@ package com.ga.app1.controller;
 import java.net.InetAddress;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.concurrent.Future;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,21 +20,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="/app1")
 public class AppController {
+
+	private static final Log logger = LogFactory.getLog(AppController.class);
+
 	
 	@Autowired
 	Environment env;
 	
+	@Async(value = "asyncExecutor")
 	@GetMapping("/details")
 	@ResponseBody
-	public Map<String,String> getTasks() throws Exception {
+	public Future<Map<String,String>> getTasks() throws Exception {
 		
-		System.out.println("invoking endpoint...");
+		logger.info("invoking endpoint...");
+		Thread.sleep(10000);
 		LinkedHashMap<String, String> map = new LinkedHashMap<String, String>();	
 		map.put("status", "success");
 		map.put("host", InetAddress.getLocalHost().getHostName());
 		map.put("port", env.getProperty("server.port"));
 		map.put("appName", env.getProperty("spring.application.name"));
-		return map;
+		logger.info("finished processing...");
+		
+		return new AsyncResult<Map<String, String>>(map);
 	
 	}
 }
